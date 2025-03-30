2705960f1b16f4296ee792c21f25ba578cb01f72
Alirea10 <2981883527@qq.com>
2025-03-30 22:54:14 +0800

    first commit

---
+++ en/index.search.js
@@ -0,0 +1,26 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "AlireaLibrary",
+    "uri": "/en/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/en/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/en/tags/index.html"
+  }
+]
